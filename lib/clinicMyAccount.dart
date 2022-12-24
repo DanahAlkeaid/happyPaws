64ceff9bc4c7e1d31705ee52c8ac69510dfac508
TheDanah <104274967+TheDanah@users.noreply.github.com>
2022-12-24 11:38:28 +0300

    delete class

--- lib/clinicMyAccount.dart
+++ lib/clinicMyAccount.dart
@@ -3,7 +3,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:rflutter_alert/rflutter_alert.dart';
-
 import 'clinic_home.dart';
 import 'FirstScreen.dart';
 import 'clinic_services.dart';
