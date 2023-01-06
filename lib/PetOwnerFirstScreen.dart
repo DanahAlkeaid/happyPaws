86392c10bba3183623fafb0ca1509eb772642e6e
DaliaAziz <120387151+DaliaAziz@users.noreply.github.com>
2023-01-06 19:11:43 +0300

    search

--- lib/PetOwnerFirstScreen.dart
+++ lib/PetOwnerFirstScreen.dart
@@ -283,7 +283,7 @@ class _ReportStatus extends State<Report> {
 
   SearchingPost(String textEntered) {
     list = FirebaseFirestore.instance
-        .collection('usrs')
+        .collection('users')
         .where('type',isEqualTo: 'clinic')
         .where('firstname', isGreaterThanOrEqualTo: textEntered)
         .get();
