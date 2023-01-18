8f5ce2d83d6cca0c1de1f0110f63e86399dd83dd
DaliaAziz <120387151+DaliaAziz@users.noreply.github.com>
2023-01-18 17:43:16 +0300

    add new class AndroidChannel

--- lib/main.dart
+++ lib/main.dart
@@ -4,14 +4,37 @@ import 'petOwnerSignup.dart';
 import 'signupScreen.dart';
 import 'FirstScreen.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'AndroidNotificationChannel.dart';
 
 //hello world
 //الووووووو
 
+class NavigationService {
+  static GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+}
+
+const AndroidNotificationChannel channel = AndroidNotificationChannel(
+  'high_importance_channel', // id
+  'High Importance Notifications', // title // description
+  importance: Importance.max,
+);
+
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+
+  await NotificationService().init(); //
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+  FlutterLocalNotificationsPlugin();
+  await flutterLocalNotificationsPlugin
+      .resolvePlatformSpecificImplementation<
+      AndroidFlutterLocalNotificationsPlugin>()
+      ?.createNotificationChannel(channel);
+
   runApp(MaterialApp(home: FirstScreen()));
+
 }
 
 
